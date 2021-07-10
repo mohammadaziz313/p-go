3d809e40be6d0da1fe58ddac9ce75d549e9e85ea
Aziz Mohammad <azizmd313@gmail.com>
2021-07-10 11:03:46 +0600

    using slice and random numbers.

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
+	"time"
 )
 
 // This is a function
@@ -15,7 +17,7 @@ func Hello(name string) (string,error){
   }
   // message variable is dynamically typeset
   // := operator is used for declaring and initializing
-  message := fmt.Sprintf("Hi, %v. Welcome!",name)
+  message := fmt.Sprintf(selectRandomFormat(),name)
   // so normally this would be the case for variable declaration
   var tmp string
   tmp = "GOはなんですか。"
@@ -23,4 +25,19 @@ func Hello(name string) (string,error){
   fmt.Println(tmp)
   // the nil specifies the error if it did not occur
   return message,nil
+}
+
+// sets initial value of rand
+func init(){
+  rand.Seed(time.Now().UnixNano())
+}
+
+// returns random quote
+func selectRandomFormat() string{
+  formats := []string{
+	"Hi, %v. Welcome!",
+	"Great to see you, %v!",
+	"Whatever %v!",
+  }
+  return formats[rand.Intn(len(formats))]
 }
